e5bd34b9f8ab500d1f5903ac031776481a102e68
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-02 15:34:41 +1000

    lazy load images

--- src/app/pages/Gallery/Gallery.js
+++ src/app/pages/Gallery/Gallery.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
-import { Loader } from "../../layout/Loader/Loader";
 import "./Gallery.css";
 const Gallery = ({ width, collection }) => {
-  const [loadedCount, setLoadedCount] = useState(100);
+  const [loadedCount, setLoadedCount] = useState(1);
 
   const incrementCount = () => {
     setLoadedCount(loadedCount + 1);
   };
+
+  useEffect(() => {
+    setLoadedCount(1);
+  }, [collection]);
 
   return (
     <>
@@ -23,23 +27,12 @@ const Gallery = ({ width, collection }) => {
             },
           }}
         >
-          {loadedCount < collection.length && (
-            <>
-              <div style={{ paddingTop: "10rem" }}></div>
-              <Loader progress={loadedCount / collection.length} />
-            </>
-          )}
-          <div
-            style={{
-              transition: "ease 0.2s",
-              visibility: loadedCount < collection.length && "hidden",
-            }}
-            className="gallery__grid"
-          >
+          <div className="gallery__grid">
             {collection.map((image) => (
               <a className="gallery__grid-image" href={image}>
-                <img
-                  width={width}
+                <LazyLoadImage
+                  effect="blur"
+                  width={"100%"}
                   src={image}
                   alt="sugma"
                   onLoad={incrementCount}
